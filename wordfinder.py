55ed42c88875ee6c1e0bef96d8b4e718e06c203f
Rajayne <ryann.j.nealey@gmail.com>
2023-02-08 12:07:36 -0800

    Created constructor for filename, function to read file and save text content, then function to randomly generate word from text content while removing line separators

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,29 @@
 """Word Finder: finds random words from a dictionary."""
-
-
+import random
 class WordFinder:
-    ...
+    """Class to read file and find words
+    
+    >>> smallWords = WordFinder("words.txt")
+
+    >>> smallWords.read_file()
+    
+    >>> smallWords.random()
+    
+    """
+    # Class that contains filename and file
+    def __init__(self, filename):
+        self.filename = filename
+
+    # Assigns value to file on self class
+    def read_file(self):
+        file = open(self.filename, "r")
+        self.allText = file.readlines()
+        self.length = len(self.allText)
+        # print(f"File read and closed, length of file: {length}")
+    
+    # Finds random word in file using random module method 
+    def random(self):
+        random_index = random.randint(0, self.length - 1)
+        print("I am attempting to read")
+        random_word = self.allText[random_index]
+        return random_word.replace("\n", "")
